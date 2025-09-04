a35a8a946f741005e1da71c7b499a55e3ae15022
Sagar-Kap <69609200+Sagar-Kap@users.noreply.github.com>
2025-09-04 07:54:53 +0300

    Initial commit

---
+++ makeguns/makeguns.cpp
@@ -0,0 +1,12 @@
+﻿// makeguns.cpp : Defines the entry point for the application.
+//
+
+#include "makeguns.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
