d694d82f1f9b32280a50e1152c784feb45740640
Sagar-Kap <69609200+Sagar-Kap@users.noreply.github.com>
2025-09-11 10:14:49 +0300

    chore: main sdl loop to create sdl window

--- makeguns/makeguns.cpp
+++ makeguns/makeguns.cpp
@@ -8,17 +8,46 @@
 
 using namespace std;
 
+void cleanup(SDL_Window *win);
+
 int main(int argc, char *argv[]) {
 
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
                              "Error initializing SDL", nullptr);
+    return 1;
   }
 
+  // create the Window
   int width = 800;
   int height = 600;
   SDL_Window *win = SDL_CreateWindow("Make Guns", width, height, 0);
 
+  if (!win) {
+
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
+                             "Error creating window", win);
+    cleanup(win);
+    return 1;
+  }
+
+  // start the game loop
+  bool running = true;
+  while (running) {
+
+    SDL_Event event{0};
+    while (SDL_PollEvent(&event)) {
+
+      switch (event.type) {
+      case SDL_EVENT_QUIT: {
+        running = false;
+        break;
+      }
+      }
+    }
+  }
+
+  cleanup(win);
   return 0;
 }
 
