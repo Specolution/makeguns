ac2ef7dd3f61b7db98e9e1888fae6bf88c805dbc
Sagar-Kap <69609200+Sagar-Kap@users.noreply.github.com>
2025-09-13 16:16:53 +0300

    chore: add sdl3_image package to the project

--- makeguns/makeguns.cpp
+++ makeguns/makeguns.cpp
@@ -1,10 +1,10 @@
 ﻿// makeguns.cpp : Defines the entry point for the application.
 //
 
+#include "makeguns.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
-#include "makeguns.h"
+#include <SDL3_image/SDL_image.h>
 
 using namespace std;
 
