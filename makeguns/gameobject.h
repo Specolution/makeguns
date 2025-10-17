273ec9cf1741be8b76157248c82f079b3455e6a4
Sagar-Kap <69609200+Sagar-Kap@users.noreply.github.com>
2025-10-17 06:25:57 +0300

    chore: gameObject type specific data union

--- makeguns/gameobject.h
+++ makeguns/gameobject.h
@@ -4,17 +4,34 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+enum class PLayerState { idle, running, jumping };
+
+struct PlayerData {
+  PLayerState state;
+  PlayerData() { state = PLayerState::idle; }
+};
+
+struct LevelData {};
+struct EnemyData {};
+
+union ObjectData {
+  PlayerData player;
+  LevelData level;
+  EnemyData enemy;
+};
+
 enum class ObjectType { player, level, enemy };
 
 struct GameObject {
   ObjectType type;
+  ObjectData data;
   glm::vec2 position, velocity, acceleration;
   float direction;
   std::vector<Animation> animations;
   int currentAnimation;
   SDL_Texture *texture;
 
-  GameObject() {
+  GameObject() : data{.level = LevelData()} {
     type = ObjectType::level;
     direction = 1;
     position = velocity = acceleration = glm::vec2(0);
